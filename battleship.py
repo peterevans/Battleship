4a0fc686da259aaf5e0f0d4860a6d75ee1be1979
peterevans <pevans@fwparker.org>
2017-03-22 17:16:07 -0500

    starting over

--- battleship.py
+++ battleship.py
@@ -1,3 +1,9 @@
+from board import Board, print_board_heading, print_board
+from player import Player
+from ship import Ship
+import os
+
+
 SHIP_INFO = [
     ("Aircraft Carrier", 5),
     ("Battleship", 4),
@@ -6,28 +12,16 @@ SHIP_INFO = [
     ("Patrol Boat", 2)
 ]
 
-BOARD_SIZE = 10
 
-VERTICAL_SHIP = '|'
-HORIZONTAL_SHIP = '-'
-EMPTY = 'O'
-MISS = '.'
-HIT = '*'
-SUNK = '#'
+if __name__ == "__main__":
+    aircraft_carrier = Ship("Aircraft Carrier", 5)
+    battleship = Ship("Battleship", 4)
+    submarine = Ship("Submarine", 3)
+    cruiser = Ship("Cruiser", 3)
+    patrol_boat = Ship("Patrol Boat", 2)
 
-
-def clear_screen():
-    print("\033c", end="")
-
-
-def print_board_heading():
-    print("   " + " ".join([chr(c) for c in range(ord('A'), ord('A') + BOARD_SIZE)]))
-
-
-def print_board(board):
-    print_board_heading()
-
-    row_num = 1
-    for row in board:
-        print(str(row_num).rjust(2) + " " + (" ".join(row)))
-        row_num += 1
+    player_name = raw_input("Player 1, enter your name: ")
+    player1 = Player(1, player_name)
+    player1_board = Board()
+    player1_opponent_board = Board()
+    print_board(player1_board.grid)
